0785dc8a3cb8f420490b971f549d9daf68c4a9d8
keegantu <keegantu@gmail.com>
2025-12-02 23:22:05 -0500

    Updated tag class

--- app/src/main/java/com/softmeth/androidphotos/models/Tag.java
+++ app/src/main/java/com/softmeth/androidphotos/models/Tag.java
@@ -1,4 +1,44 @@
-package com.softmeth.androidphotos.models;
+package com.softmeth.photosandroid.models;
 
-public class Tag {
+import java.io.Serializable;
+
+/**
+ * Represents a tag with name-value pair.
+ */
+public class Tag implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String name;
+    private String value;
+    
+    public Tag(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getValue() {
+        return value;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Tag tag = (Tag) obj;
+        return name.equalsIgnoreCase(tag.name) && value.equalsIgnoreCase(tag.value);
+    }
+    
+    @Override
+    public int hashCode() {
+        return (name.toLowerCase() + value.toLowerCase()).hashCode();
+    }
+    
+    @Override
+    public String toString() {
+        return name + ": " + value;
+    }
 }
